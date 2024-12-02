61e48c9d79a8951225684374da99151890b94854
Orphereum <josemcr28012001@gmail.com>
2024-12-02 09:55:27 +0100

    RespuestasImplementadas
    Respuestas javascript

--- javascript.js
+++ javascript.js
@@ -1,10 +1,66 @@
 function comprobarRespuesta() {
     // Respuestas correctas (se asignan según tus criterios)
     const respuestasCorrectas = {
-      q1: '1',  // Pregunta 1, opción 1 correcta
-      q2: '2',  // Pregunta 2, opción 2 correcta
+      q1: '2',  // Pregunta 1, opción 1 correcta
+      q2: '3',  // Pregunta 2, opción 2 correcta
       q3: '3',  // Pregunta 3, opción 3 correcta
-      q4: '2'   // Pregunta 4, opción 2 correcta
+      q4: '2',  // Pregunta 4, opción 2 correcta
+      q5: '2',
+      q6: '1',
+      q7: '3',
+      q8: '2',
+      q9: '2',
+      q10: '4',
+      q11: '2',
+      q12: '2',
+      q13: '4',
+      q14: '2',
+      q15: '1',
+      q16: '2',
+      q17: '3',
+      q18: '2',
+      q19: '1',
+      q20: '4',
+      q21: '1',
+      q22: '1',
+      q23: '2',
+      q24: '3',
+      q25: '2',
+      q26: '2',
+      q27: '1',
+      q28: '2',
+      q29: '1',
+      q30: '3',
+      q31: '2',
+      q32: '1',
+      q33: '2',
+      q34: '1',
+      q35: '3',
+      q36: '1',
+      q37: '3',
+      q38: '2',
+      q39: '1',
+      q40: '3',
+      q41: '1',
+      q42: '3',
+      q43: '3',
+      q44: '1,3',
+      q45: '1,3',
+      q46: '3',
+      q47: '1',
+      q48: '2',
+      q49: '3',
+      q50: '4',
+      q51: '2',
+      q52: '3',
+      q53: '1',
+      q54: '2',
+      q55: '1',
+      q56: '1',
+      q57: '3',
+      q58: '2',
+      q59: '4',
+      q60: '2',
     };
   
     let respuestasCorrectasContadas = 0;
